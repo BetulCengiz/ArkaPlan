0b8bb75be0d2e276e7838fac0818d157f8e0e7a1
BetulCengiz <bc199911@hotmail.com>
2021-02-16 19:11:59 +0300

    Add project files.

---
+++ ArkaPlan/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ArkaPlan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+        
+            //Console.BackgroundColor = ConsoleColor.DarkRed;
+            //Console.ForegroundColor = ConsoleColor.Yellow;
+            //Console.Clear();
+            //Console.Title = "Başlık";
+            //Console.WriteLine("Merhaba bu bir arka plan denemesidir");
+            // boxing and unboxing
+
+            Console.Write("0'dan 15'e kadar bir  sayı giriniz");
+            ConsoleColor renk = (ConsoleColor)Convert.ToInt16(Console.ReadLine());
+            Console.BackgroundColor = renk;
+            Console.Clear();
+
+
+        }
+    }
+}
